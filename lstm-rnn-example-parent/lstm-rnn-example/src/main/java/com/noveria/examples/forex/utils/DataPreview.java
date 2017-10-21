df7cc0ebf5d33f23760a63144d215f41bd224c71
noveria <jaybono30@hotmail.com>
2017-10-21 21:12:21 +0100

    Fixed Apache Spark Dependancies

--- lstm-rnn-example-parent/lstm-rnn-example/src/main/java/com/noveria/examples/forex/utils/DataPreview.java
+++ lstm-rnn-example-parent/lstm-rnn-example/src/main/java/com/noveria/examples/forex/utils/DataPreview.java
@@ -12,28 +12,37 @@ import java.io.IOException;
 public class DataPreview {
     public static void main (String[] args) throws IOException {
         SparkSession spark = SparkSession.builder().master("local").appName("DataProcess").getOrCreate();
-        String filename = "prices-split-adjusted.csv";
+        //String filename = "prices-split-adjusted.csv";
+        String filename = "GBP_USD_testData.csv";
+
         String symbol = "GOOG";
         // load data from csv file
 
-        System.out.println(new ClassPathResource(filename).getFile().getAbsolutePath());
+        //date,symbol,open,close,low,high,volume
+        //instrument,open,high,low,close,date
+
+        //System.out.println(new ClassPathResource(filename).getFile().getAbsolutePath());
+        //2012-04-30:01:00:00+0100
+
+
         Dataset<Row> data = spark.read().format("csv").option("header", true)
-                .option("timestampFormat", "yyyy/MM/dd HH:mm:ss ZZ")
+                //.option("timestampFormat", "yyyy/MM/dd HH:mm:ss ZZ")
+                .option("timestampFormat", "yyyy-MM-dd:HH:mm:ss ZZ")
                 .load(new ClassPathResource(filename).getFile().getAbsolutePath())
                 //.filter(functions.col("symbol").equalTo(symbol))
                 //.drop("date").drop("symbol")
+                .withColumn("instrument", functions.col("instrument").cast("string"))
                 .withColumn("openPrice", functions.col("open").cast("double")).drop("open")
-                .withColumn("closePrice", functions.col("close").cast("double")).drop("close")
-                .withColumn("lowPrice", functions.col("low").cast("double")).drop("low")
                 .withColumn("highPrice", functions.col("high").cast("double")).drop("high")
-                .withColumn("volumeTmp", functions.col("volume").cast("double")).drop("volume")
-                .toDF("date", "symbol", "open", "close", "low", "high", "volume");
+                .withColumn("lowPrice", functions.col("low").cast("double")).drop("low")
+                .withColumn("closePrice", functions.col("close").cast("double")).drop("close")
+                .toDF("instrument", "date", "open", "high", "low", "close");
 
         data.show();
 
-        Dataset<Row> symbols = data.select("date", "symbol").groupBy("symbol").agg(functions.count("date").as("count"));
-        System.out.println("Number of Symbols: " + symbols.count());
-        symbols.show();
+        Dataset<Row> instruments = data.select("date", "instrument").groupBy("instrument").agg(functions.count("date").as("count"));
+        System.out.println("Number of Symbols: " + instruments.count());
+        instruments.show();
 
 //        VectorAssembler assembler = new VectorAssembler()
 //                .setInputCols(new String[] {"open", "low", "high", "volume", "close"})
